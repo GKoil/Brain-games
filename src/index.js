07b59576eb82d0b080942ba1180a831b72228f9f
Кирилл <samsuu@ya.ru>
2020-03-07 21:56:15 +0700

    step 4 | fix eslint, readme

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 
 import readlineSync from 'readline-sync';
 
-
 const getUserName = () => {
   const userName = readlineSync.question('May I have your name?');
   console.log(`Hello, ${userName}!`);
