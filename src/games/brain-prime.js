aad2ec65d4731591e7061b5cca269d08aad8a58f
Кирилл <samsuu@ya.ru>
2020-05-07 09:04:11 +0700

    fix #4.1 | update old name function

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,12 +1,12 @@
 import runGameBasis from '../index.js';
-import getRandomValue from '../utils.js';
+import getRandom from '../utils.js';
 
 const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (value) => (value % 2 === 0 || value % 3 === 0 || value % 5 === 0) && value > 2;
 
 const getRandomNumber = () => {
-  const question = getRandomValue(0, 3);
+  const question = getRandom(0, 3);
   const answerQuestion = isPrime(question);
   const answer = answerQuestion === true ? 'yes' : 'no';
 
